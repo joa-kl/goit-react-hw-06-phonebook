22bdabf06178a9a961faa954bf403e6f8c2c3244
Joanna K <joannakleinrm@gmail.com>
2023-07-07 19:51:49 +0200

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,13 +4,10 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { contactsFilterSlice } from "./slices/filterSlice";
 import { contactsItemsSlice } from "./slices/contactSlice";
 
-
-
 const contactsReducer = combineReducers({
     [contactsItemsSlice.name]: contactsItemsSlice.reducer,
     [contactsFilterSlice.name]: contactsFilterSlice.reducer,
 });
-
 
 export const reducer = {
     contacts: contactsReducer,
