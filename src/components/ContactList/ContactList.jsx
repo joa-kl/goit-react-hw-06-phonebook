847509252b3e972da2bcd0dfb6379a1ff218ce5f
Joanna K <joannakleinrm@gmail.com>
2023-07-07 19:50:21 +0200

    selectors created

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,28 +3,20 @@ import css from './ContactList.module.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { handleRemoveContact } from 'redux/slices/contactSlice';
 import { Notify } from 'notiflix';
+import { selectContacts, selectFilter } from 'redux/selectors';
 
 export const ContactList = () => {
-
-
-  const contacts = useSelector(state => state.contacts.items);
-  const filterValue = useSelector(state => state.contacts.filter);
-
-  const isVisibleContacts = () => 
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterValue.toLowerCase()),
-    );
-  
-  const filterContact = isVisibleContacts();
-
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
-  
+
+  const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
 
   return (
 
     <div>
       <ul>
-        {filterContact.map((contact) => (
+        {filteredContacts.map((contact) => (
           <li key={contact.id} className={css.contactListItem}>
             {contact.name}: {contact.number}
             <button
@@ -33,7 +25,7 @@ export const ContactList = () => {
               onClick={() => 
                 dispatch(
                   handleRemoveContact(contact.id),
-                  Notify.success("Contact was deleted"),
+                  Notify.warning("Contact was deleted"),
               )}
             >
               Delete
